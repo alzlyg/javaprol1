9e49bd71f5f47eda0912a5cc019ee50bbd024a08
Alexey <webmaster.innobi@gmail.com>
2018-10-18 16:18:39 +0300

    Домашнее задание к первому уроку

---
+++ src/main/java/ru/zlygostev/Apple.java
@@ -0,0 +1,10 @@
+package ru.zlygostev;
+
+// Класс яблоки
+public class Apple extends Fruit {
+
+    public void Apple() {
+        setType(FruitType.APPLE);
+        setWeigth(1);
+    }
+}
---
+++ src/main/java/ru/zlygostev/Orange.java
@@ -0,0 +1,10 @@
+package ru.zlygostev;
+
+// Класс апельсины
+public class Orange extends Fruit {
+
+    public void Orange() {
+        setType(FruitType.ORANGE);
+        setWeigth(1.5);
+    }
+}
